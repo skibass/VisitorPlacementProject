72c51dba8158d2e9ffca1d1146a5c1079cab1659
Thijn Senden <508700@student.fontys.nl>
2024-05-06 01:31:28 +0200

    refractured methods for editing events with the purpose of better readability and seperation of concerns.

--- VPTExtra/DataAcces/EventEditRepository.cs
+++ VPTExtra/DataAcces/EventEditRepository.cs
@@ -20,127 +20,125 @@ namespace DataAcces
 
         public Part AddPart(int eventId)
         {
-
             db.Open();
 
-            MySqlCommand getMaxPartNameCmd = new MySqlCommand("SELECT MAX(name) AS maxPartName FROM part WHERE event_id = @eventId;", db);
-            getMaxPartNameCmd.Parameters.AddWithValue("@eventId", eventId);
-
-            string maxPartName = getMaxPartNameCmd.ExecuteScalar() as string;
-            char nextPartLetter = 'A';
-
-            if (!string.IsNullOrEmpty(maxPartName))
-            {
-                char lastLetter = maxPartName[maxPartName.Length - 1];
-                nextPartLetter = (char)(lastLetter + 1);
-            }
+            string maxPartName = GetMaxPartName(eventId);
+            char nextPartLetter = GetNextPartLetter(maxPartName);
 
             string nextPartName = nextPartLetter.ToString();
-
-            MySqlCommand insertPartCmd = new MySqlCommand("INSERT INTO part (name, event_id) VALUES (@partName, @eventId);", db);
-            insertPartCmd.Parameters.AddWithValue("@partName", nextPartName);
-            insertPartCmd.Parameters.AddWithValue("@eventId", eventId);
-
-            insertPartCmd.ExecuteNonQuery();
+            InsertPart(eventId, nextPartName);
 
             db.Close();
 
-            Part newPart = new Part
-            {
-                Name = nextPartName
-            };
-
-            return newPart;
+            return new Part { Name = nextPartName };
         }
 
         public Row AddRow(int partId)
         {
             db.Open();
 
-            MySqlCommand getMaxRowNumberCmd = new MySqlCommand("SELECT MAX(name) AS maxRowNumber FROM row WHERE part_id = @partId;", db);
-            getMaxRowNumberCmd.Parameters.AddWithValue("@partId", partId);
-
-            object maxRowNumberObj = getMaxRowNumberCmd.ExecuteScalar();
-
-            string maxRowString = maxRowNumberObj.ToString();
-
-            int nextRowNumber;
-            if (maxRowString.Length >= 2)
-            {
-                string numberString = maxRowString.Substring(1);
-                nextRowNumber = int.Parse(numberString) + 1;
-            }
-            else
-            {
-                nextRowNumber = 1;
-            }
-
-            MySqlCommand getPartLetterCmd = new MySqlCommand("SELECT name FROM part WHERE id = @partId;", db);
-            getPartLetterCmd.Parameters.AddWithValue("@partId", partId);
-
-            string partLetter = getPartLetterCmd.ExecuteScalar() as string;
+            string partLetter = GetPartLetter(partId);
+            int nextRowNumber = GetNextRowNumber(partId);
 
             string nextRowName = $"{partLetter}{nextRowNumber}";
-
-            MySqlCommand insertRowCmd = new MySqlCommand("INSERT INTO row (name, part_id) VALUES (@rowName, @partId);", db);
-            insertRowCmd.Parameters.AddWithValue("@rowName", nextRowName);
-            insertRowCmd.Parameters.AddWithValue("@partId", partId);
-
-            insertRowCmd.ExecuteNonQuery();
+            InsertRow(partId, nextRowName);
 
             db.Close();
 
-            Row newRow = new Row
-            {
-                Name = nextRowName,
-            };
-
-            return newRow;
+            return new Row { Name = nextRowName };
         }
 
         public Chair AddChair(int rowId, int eventId)
         {
             db.Open();
 
-            MySqlCommand getMaxChairNumberCmd = new MySqlCommand("SELECT MAX(name) AS maxChairName FROM chair WHERE row_id = @rowId;", db);
-            getMaxChairNumberCmd.Parameters.AddWithValue("@rowId", rowId);
-
-            object maxChairNumberObj = getMaxChairNumberCmd.ExecuteScalar();
-            int nextChairNumber = 1;
-
-            if (maxChairNumberObj != DBNull.Value)
-            {
-                string maxChairName = maxChairNumberObj.ToString();
-                string[] chairNameSplit = maxChairName.Split('-');
-                nextChairNumber = int.Parse(chairNameSplit[1]) + 1;
-            }
-
-            MySqlCommand getRowNameCmd = new MySqlCommand("SELECT name FROM row WHERE id = @rowId;", db);
-            getRowNameCmd.Parameters.AddWithValue("@rowId", rowId);
-
-            string rowName = getRowNameCmd.ExecuteScalar().ToString();
+            int nextChairNumber = GetNextChairNumber(rowId);
+            string rowName = GetRowName(rowId);
 
             string nextChairName = $"{rowName}-{nextChairNumber}";
-
-            MySqlCommand insertChairCmd = new MySqlCommand("INSERT INTO chair (name, row_id) VALUES (@name, @rowId);", db);
-            insertChairCmd.Parameters.AddWithValue("@name", nextChairName);
-            insertChairCmd.Parameters.AddWithValue("@rowId", rowId);
-
-            insertChairCmd.ExecuteNonQuery();
-
-            MySqlCommand updateVisitorLimitCmd = new MySqlCommand("UPDATE event SET visitorLimit = visitorLimit + 1 WHERE id = @eventId;", db);
-            updateVisitorLimitCmd.Parameters.AddWithValue("@rowId", rowId);
-            updateVisitorLimitCmd.Parameters.AddWithValue("@eventId", eventId);
-            updateVisitorLimitCmd.ExecuteNonQuery();
+            InsertChair(rowId, nextChairName, eventId);
 
             db.Close();
 
-            Chair newChair = new Chair
-            {
-                Name = nextChairName,
-            };
+            return new Chair { Name = nextChairName };
+        }
 
-            return newChair;
+        private string GetMaxPartName(int eventId)
+        {
+            var cmd = new MySqlCommand("SELECT MAX(name) AS maxPartName FROM part WHERE event_id = @eventId;", db);
+            cmd.Parameters.AddWithValue("@eventId", eventId);
+            return cmd.ExecuteScalar() as string;
+        }
+
+        private char GetNextPartLetter(string maxPartName)
+        {
+            char nextPartLetter = 'A';
+            if (!string.IsNullOrEmpty(maxPartName))
+            {
+                char lastLetter = maxPartName[maxPartName.Length - 1];
+                nextPartLetter = (char)(lastLetter + 1);
+            }
+            return nextPartLetter;
+        }
+
+        private void InsertPart(int eventId, string partName)
+        {
+            var cmd = new MySqlCommand("INSERT INTO part (name, event_id) VALUES (@partName, @eventId);", db);
+            cmd.Parameters.AddWithValue("@partName", partName);
+            cmd.Parameters.AddWithValue("@eventId", eventId);
+            cmd.ExecuteNonQuery();
+        }
+
+        private string GetPartLetter(int partId)
+        {
+            var cmd = new MySqlCommand("SELECT name FROM part WHERE id = @partId;", db);
+            cmd.Parameters.AddWithValue("@partId", partId);
+            return cmd.ExecuteScalar().ToString();
+        }
+
+        private int GetNextRowNumber(int partId)
+        {
+            var cmd = new MySqlCommand("SELECT MAX(name) AS maxRowNumber FROM row WHERE part_id = @partId;", db);
+            cmd.Parameters.AddWithValue("@partId", partId);
+
+            string maxRowString = cmd.ExecuteScalar().ToString();
+            return maxRowString.Length >= 2 ? int.Parse(maxRowString.Substring(1)) + 1 : 1;
+        }
+
+        private void InsertRow(int partId, string rowName)
+        {
+            var cmd = new MySqlCommand("INSERT INTO row (name, part_id) VALUES (@rowName, @partId);", db);
+            cmd.Parameters.AddWithValue("@rowName", rowName);
+            cmd.Parameters.AddWithValue("@partId", partId);
+            cmd.ExecuteNonQuery();
+        }
+
+        private int GetNextChairNumber(int rowId)
+        {
+            var cmd = new MySqlCommand("SELECT MAX(name) AS maxChairName FROM chair WHERE row_id = @rowId;", db);
+            cmd.Parameters.AddWithValue("@rowId", rowId);
+
+            object maxChairNumberObj = cmd.ExecuteScalar();
+            return maxChairNumberObj != DBNull.Value ? int.Parse(maxChairNumberObj.ToString().Split('-')[1]) + 1 : 1;
+        }
+
+        private string GetRowName(int rowId)
+        {
+            var cmd = new MySqlCommand("SELECT name FROM row WHERE id = @rowId;", db);
+            cmd.Parameters.AddWithValue("@rowId", rowId);
+            return cmd.ExecuteScalar().ToString();
+        }
+
+        private void InsertChair(int rowId, string chairName, int eventId)
+        {
+            var cmd = new MySqlCommand("INSERT INTO chair (name, row_id) VALUES (@name, @rowId);", db);
+            cmd.Parameters.AddWithValue("@name", chairName);
+            cmd.Parameters.AddWithValue("@rowId", rowId);
+            cmd.ExecuteNonQuery();
+
+            var updateVisitorLimitCmd = new MySqlCommand("UPDATE event SET visitorLimit = visitorLimit + 1 WHERE id = @eventId;", db);
+            updateVisitorLimitCmd.Parameters.AddWithValue("@eventId", eventId);
+            updateVisitorLimitCmd.ExecuteNonQuery();
         }
     }
 }
